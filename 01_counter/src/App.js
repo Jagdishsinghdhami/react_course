d4d65a0ba25deb534221e63d596ba0b50f1881c2
Jagdishsinghdhami <jagdishdhami64@gmail.com>
2023-12-23 19:58:22 +0530

    react first commit.

--- 01_counter/src/App.js
+++ 01_counter/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    if (counter <= 20) {
+      setCounter(counter + 1);
+      console.log(counter);
+    } else {
+      setCounter(counter);
+      console.log(`after 20 value is not increased`);
+    }
+  };
+  const handleMinusClick = () => {
+    if (counter >= 0) {
+      setCounter(counter - 1);
+      console.log(`counterminus ${counter}`);
+    } else {
+      setCounter(counter);
+      console.log(`minus value is not acceptable`);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React course</h1>
+      <h1>counter value : {counter}</h1>
+
+      <button onClick={handleClick}>Add Value</button>
+      <br />
+      <button onClick={handleMinusClick}>Minus Value</button>
+    </>
   );
 }
 
